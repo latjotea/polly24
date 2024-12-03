c8427c9958786f60db142c82e91e649043690107
Yraolsenobrink <yra.olsen-obrink-1327@skola.uu.se>
2024-12-03 11:55:33 +0100

    fixat lite på destinationview

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,10 +1,10 @@
 <template>
-
     <body>
-        <p> hej</p>
-        <div>
-        <button v-on:click="navigateToCreate">
-          {{ uiLabels.createCrawl }}
+      <h1>{{ uiLabels.goTo }}</h1>
+
+      <div id="goto">
+        <button>
+          {{ uiLabels.imHere }}
         </button>
     </div>
     </body>
@@ -12,48 +12,45 @@
 
 
 <script>
-
+import io from 'socket.io-client';
+const socket = io("localhost:3000");
 import Pubs from '/server/data/Pubs.json';
 
 export default{
     name:"DestinationView",
     
-
 data: function () {
     return {
       uiLabels: {},
-     
+      selectedPubs: [], /*koppla över socket till publistview*/
+      teamPubs: [] 
     }
-  }
+  },
+  created: function () {
+    socket.on( "uiLabels", labels => this.uiLabels = labels );
+    socket.emit( "getUILabels", this.lang );
+  },
 }
-
 </script>
 
 
-
-
-
 <style>
-
 body{
   margin-top: 12rem;
   background-color:rgb(255, 240, 245);
   font-family: 'Galindo';
   height: 100vh;
+  font-size: 2rem;
   }
 
-header h1{
-    color: black;
-}
-button {
+#goto button{
     font-size: 2rem;
     font-family: 'Galindo';
     background-color: rgb(65, 105, 225);
     cursor:pointer;
   }
-  button:hover{
+
+#goto button:hover{
     color:white;
   }
-
-
 </style>
