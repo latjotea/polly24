4e3c2308ddb2261af0fa8ae103dca33801bbbf2d
hedvigejderhamn <hedvig.er@gmail.com>
2024-12-10 17:13:59 +0100

    Klar med team building

--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -1,7 +1,7 @@
-/* TEA OCH EMMA ANSVARIG FÖR SENASTE VERSIONEN */
+
 <template>
   <div>
-    <div id="crawlInfo">
+    <div v-if="!shuffle" id="crawlInfo">
       <span>{{this.uiLabels.crawlID}}</span> {{ crawlId }}
     </div>
     <div v-if="!joined">
@@ -15,12 +15,36 @@
         </button>
       </div>
     </div>
-    <div v-if="joined">
+    <div v-if="joined && !shuffle">
       <p>Waiting for host to start poll</p>
       <div v-for="person in participants" :key="person.name">
        {{ person.name }} 
       </div>
     </div>
+
+    <div>
+      <button v-if="joined && !shuffle" v-on:click="shuffleTeam" id="shuffle">
+        {{ this.uiLabels.shuffle }}
+      </button>
+    </div>
+
+    <div v-if="shuffle">
+      <h3>{{this.uiLabels.teamDivision}}</h3>
+      <div v-for="(team, index) in teams" :key="index" class="team-container">
+        <h4>{{ this.uiLabels.team }} {{ index + 1 }}</h4>
+        <ul>
+          <li v-for="member in team" :key="member.name">{{ member.name }}</li>
+        </ul>
+      </div>
+    </div>
+
+    <div v-if="shuffle">
+      <button id="start">
+        {{ this.uiLabels.start }}
+      </button>
+
+    </div>
+
   </div>
 </template>
 
@@ -37,22 +61,52 @@ export default {
       uiLabels: {},
       joined: false,
       lang: localStorage.getItem("lang") || "en",
-      participants: []
+      participants: [],
+      teams: [],
+      teamAmount: '',
+      shuffle:false,
     }
   },
   created: function () {
     this.crawlId = this.$route.params.id;
     socket.on( "uiLabels", labels => this.uiLabels = labels );
+    socket.emit( "getUILabels", this.lang );
     socket.on( "participantsUpdate", p => this.participants = p );
     socket.on( "startPoll", () => this.$router.push("/poll/" + this.crawlId) );
     socket.emit( "joinPoll", this.crawlId );
-    socket.emit( "getUILabels", this.lang );
+    socket.emit("getTeamAmount", {crawlId: this.crawlId });
+
+    socket.on("selectedTeamAmountResponse", (teamAmount) => {
+    this.teamAmount = teamAmount; 
+    console.log("Antalet lag är:", this.teamAmount); 
+
+  });
+
+    
   },
   methods: {
     participateInPoll: function () {
       socket.emit( "participateInPoll", {crawlId: this.crawlId, name: this.userName} )
       this.joined = true;
-    }
+    },
+
+    //OBS finns fel med metoden - ex. går det ej att slumpa lag om endast en person är med - men kanske ej gör något
+
+    //Metod tagen från chatgpt - Använd Fisher-Yates-algoritmen för att slumpa ordningen på deltagarna
+    
+    shuffleTeam: function () {
+    // Blanda deltagarna och fördela i lag
+      this.teams = [...this.participants]
+        .sort(() => Math.random() - 0.5) // Blanda med sort och en slumpmässig faktor
+        .reduce((teams, participant, index) => {
+          teams[index % this.teamAmount].push(participant); // Fördela deltagarna jämnt
+          return teams;
+        }, 
+      Array.from({ length: this.teamAmount }, () => [])); // Skapa tomma lag
+
+      this.shuffle = true; 
+    },
+
   }
 }
 </script>
@@ -84,5 +138,32 @@ body{
     font-size:2rem;
     font-family: 'Galindo';
   }
+
+  ul {
+    list-style-type: none; 
+    padding: 0; 
+    margin: 0; 
+  }
+
+  .team-container {
+    border: 3px solid rgb(53, 90, 199); 
+    border-radius: 20px; /* Rundade hörn */
+    padding: 10px; /* Inre marginal */
+    margin: 20px auto; /* Yttre marginal, centrering med auto */
+    max-width: 80%; /* Maximal bredd för ramen */
+    background-color: rgb(65, 105, 225);; /* Bakgrundsfärg */
+  }
+
+  #shuffle {
+    margin-top: 3vh;
+  }
+
+  #start {
+    background-color: rgb(141, 242, 141);
+  }
+  
+  /* Kvar att fixa: Spara lagen som skapas och skicka på socket */
   
 </style>
+
+
